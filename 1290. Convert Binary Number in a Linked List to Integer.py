f2fb05149784288d1e8c9c8ec11b39dd612759b8
j8nya <63924696+j8nya@users.noreply.github.com>
2022-10-25 13:13:32 +0300

    Create 1290. Convert Binary Number in a Linked List to Integer.py

---
+++ 1290. Convert Binary Number in a Linked List to Integer.py
@@ -0,0 +1,12 @@
+#   a->b->c
+#   (((0*2)+a)*2+b)*2+c
+
+class Solution:
+    def getDecimalValue(self, head):
+        res = 0
+        #пока не конец списка 
+        while head:
+            res = 2 * res + head.val
+            head = head.next
+        return res
+        
